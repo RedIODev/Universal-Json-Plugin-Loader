65726e12848da0931a9cb6c057c7bf79777468be
RedIODev <redio.development@gmail.com>
2025-11-27 00:22:01 +0100

    made proc macro crate warning free

--- proc-macros/src/lib.rs
+++ proc-macros/src/lib.rs
@@ -6,26 +6,9 @@ use syn::{
     Attribute, BareFnArg, BareVariadic, FnArg, GenericParam, Generics, Ident, ItemFn, ItemImpl, ItemTrait, ItemType, PatType, Signature, Token, TraitItem, TraitItemFn, TypeBareFn, Variadic, Visibility, parse::Parse, parse_macro_input, parse_quote, punctuated::Punctuated, token::Comma
 };
 
-
-
-#[proc_macro_attribute]
-pub fn plugin_main(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let item  = parse_macro_input!(item as ItemFn);
-    let item_name = item.sig.ident.clone();
-    quote! {
-        use plugin_loader_api::{CUuid, CPluginInfo};
-        #[unsafe(no_mangle)]
-        pub unsafe extern "C" fn plugin_main(uuid: CUuid) -> CPluginInfo {
-            #item_name(uuid.into()).into()
-        }
-
-        #item
-    }.into()
-}
-
 struct AttrArgs {
-    trait_ident: Ident,
     struct_ident: Ident,
+    trait_ident: Ident,
 }
 
 impl Parse for AttrArgs {
@@ -33,20 +16,92 @@ impl Parse for AttrArgs {
         let trait_ident = input.parse()?;
         let _: Token![for] = input.parse()?;
         let struct_ident = input.parse()?;
-        Ok(AttrArgs {
-            trait_ident,
+        Ok(Self {
             struct_ident,
+            trait_ident,
         })
     }
 }
 
+trait ArrayIter: Iterator {
+    fn collect_array<const N: usize>(self) -> Result<[Self::Item;N], ArrayBoundsError> where Self: Sized{
+        let collector: ArrayCollector<_,_> = self.collect();
+        collector.0
+    }
+}
+
+impl<T> ArrayIter for T where T: Iterator + ?Sized {}
+
+struct ArrayCollector<T, const N:usize>(Result<[T;N], ArrayBoundsError>);
+
+
+impl<T, const N:usize> FromIterator<T> for ArrayCollector<T, N> {
+    #[expect(clippy::unnecessary_safety_comment, clippy::undocumented_unsafe_blocks,
+        clippy::indexing_slicing, reason = "1 safety comment explains the functions safety")]
+    fn from_iter<B: IntoIterator<Item = T>>(iter: B) -> Self {
+        // SAFETY: The result items are guaranteed to be uninit until the for loop.
+        // After the ith iteration indices from 0 to i are guaranteed to be init by passing through the if branch.
+        // In case the iterator ends before N elements got initialized the first i elements that are init get droped.
+        // If the iterator contains more then N elements all elements in result get droped.
+        // Once the iter is empty and N(all) elements in the array got initialized it is safe to map all elements with assume_init.
+        let mut result = array::from_fn::<_, N,_>(|_| MaybeUninit::<T>::uninit());
+        let mut iterator = iter.into_iter();
+        for i in 0..N {
+            let item = iterator.next();
+            if let Some(item_t) = item {
+                result[i] = MaybeUninit::new(item_t);
+            } else {
+                result.iter_mut().take(i).for_each(|init_ele| unsafe { init_ele.assume_init_drop()});
+                return Self(Err(ArrayBoundsError::Only(i, N)));
+            }
+        }
+        if iterator.next().is_some() {
+            result.iter_mut().for_each(|init_ele| unsafe { init_ele.assume_init_drop()});
+            return Self(Err(ArrayBoundsError::MoreThan(N)));
+        } 
+        Self(Ok(result.map(|init_ele| unsafe { init_ele.assume_init()})))
+    }
+}
+
+enum ArrayBoundsError {
+    MoreThan(usize),
+    Only(usize, usize),
+}
+
+impl Display for ArrayBoundsError {
+    #[expect(clippy::arithmetic_side_effects, reason = "adding 1 to display doesn't matter")]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Only(only, from) => write!(f, "Expected {from} element(s), found {only}."),
+            Self::MoreThan(from) => write!(f, "Expected {from} elements, found {} or more.", from+1)
+        }
+    }
+}
+
+
+
+#[proc_macro_attribute]
+pub fn plugin_main(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item_fn  = parse_macro_input!(item as ItemFn);
+    let item_name = item_fn.sig.ident.clone();
+    quote! {
+        use plugin_loader_api::{CUuid, CPluginInfo};
+        #[unsafe(no_mangle)]
+        pub unsafe extern "C" fn plugin_main(uuid: CUuid) -> CPluginInfo {
+            #item_name(uuid.into()).into()
+        }
+
+        #item_fn
+    }.into()
+}
+
 #[proc_macro_attribute]
 pub fn trait_fn(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let attr = parse_macro_input!(attr as AttrArgs);
-    let func = syn::parse::<ItemFn>(item);
-    let struct_name = attr.struct_ident;
-    let trait_ident = attr.trait_ident;
-    let result = match func {
+    let attr_args = parse_macro_input!(attr as AttrArgs);
+    let item_fn = syn::parse::<ItemFn>(item);
+    let struct_name = attr_args.struct_ident;
+    let trait_ident = attr_args.trait_ident;
+    let result = match item_fn {
         Ok(func) => {
             let mut impl_func = func.clone();
             impl_func.vis = syn::Visibility::Inherited;
@@ -79,6 +134,7 @@ pub fn fn_trait(_attr: TokenStream, item: TokenStream) -> TokenStream {
     }
 }
 
+#[expect(clippy::single_call_fn, reason = "function extracted for visibility")]
 fn fn_trait_result(mut item: ItemTrait) -> Result<TokenStream, TokenStream> {
     let trait_vis = &item.vis;
     let trait_name = &item.ident;
@@ -108,7 +164,7 @@ fn fn_trait_result(mut item: ItemTrait) -> Result<TokenStream, TokenStream> {
 
     //update trait elements
     let fp_adapter_ident = fp_adapter.sig.ident.clone();
-    item.items.retain(|item| if let TraitItem::Fn(item) = item { item.sig.ident != fp_adapter_ident} else {true});
+    item.items.retain(|trait_item| if let TraitItem::Fn(item_fn) = trait_item { item_fn.sig.ident != fp_adapter_ident} else {true});
     item.items.append(&mut vec![sig_fp_getter, adapter_fp_getter]);
     
     Ok(quote! {
@@ -120,6 +176,7 @@ fn fn_trait_result(mut item: ItemTrait) -> Result<TokenStream, TokenStream> {
     }.into())
 }
 
+#[expect(clippy::single_call_fn, reason = "function extracted for visibility")]
 fn sig_getter(sig_func: &TraitItemFn, sig_fn_type: &TypeBareFn) -> TraitItem {
     let sig_generics = &sig_func.sig.generics;
     let sig_func_name = &sig_func.sig.ident;
@@ -132,25 +189,27 @@ fn sig_getter(sig_func: &TraitItemFn, sig_fn_type: &TypeBareFn) -> TraitItem {
     }
 }
 
+#[expect(clippy::single_call_fn, reason = "function extracted for visibility")]
 fn adapter_getter(adapter_func: &TraitItemFn, adapter_fn_type: &TypeBareFn) ->TraitItem {
     let adapter_generics = &adapter_func.sig.generics;
     let mut call_site_generics = adapter_generics.clone();
     let adapter_func_name = &adapter_func.sig.ident;
     let adapter_getter_name = Ident::new(&format!("{}_fp", adapter_func.sig.ident), Span::call_site().into());
-    call_site_generics.params.iter_mut().for_each(|param| if let GenericParam::Type(t) = param {
-        t.colon_token = None;
-        t.bounds = Punctuated::default();
-        t.default = None;
+    call_site_generics.params.iter_mut().for_each(|param| if let GenericParam::Type(type_param) = param {
+        type_param.colon_token = None;
+        type_param.bounds = Punctuated::default();
+        type_param.default = None;
     });
-    let call_site_generics = call_site_generics.params;
+    let call_site_generic_params = call_site_generics.params;
     parse_quote! {
         #[inline]
         fn #adapter_getter_name #adapter_generics () -> #adapter_fn_type {
-            Self:: #adapter_func_name ::<#call_site_generics>
+            Self:: #adapter_func_name ::<#call_site_generic_params>
         }
     }
 }
 
+#[expect(clippy::single_call_fn, reason = "function extracted for visibility")]
 fn fp_adapter_trait(fp_adapter: &TraitItemFn, trait_name: &Ident, trait_vis: &Visibility) -> Result<(ItemTrait, ItemImpl), TokenStream> {
     
     let fp_adapter_name = &fp_adapter.sig.ident;
@@ -158,12 +217,12 @@ fn fp_adapter_trait(fp_adapter: &TraitItemFn, trait_name: &Ident, trait_vis: &Vi
     let fp_adapter_generics = &fp_adapter.sig.generics;
     let fp_adapter_return_type = &fp_adapter.sig.output;
 
-    let fp_adapter_receiver = fp_adapter.sig.inputs.first().ok_or(new_error(format!("{} must take one argument.", fp_adapter.sig.ident)))?;
-    let FnArg::Receiver(fp_adapter_receiver) = fp_adapter_receiver else {
+    let fp_adapter_arg = fp_adapter.sig.inputs.first().ok_or_else(|| new_error(format!("{} must take one argument.", fp_adapter.sig.ident)))?;
+    let FnArg::Receiver(fp_adapter_receiver) = fp_adapter_arg else {
         return Err(new_error(format!("{} must take a self parameter.", fp_adapter.sig.ident)))
     };
     let fp_adapter_receiver_type = &*fp_adapter_receiver.ty;
-    let fp_adapter_body = fp_adapter.default.as_ref().ok_or(new_error(format!("{} must have an implementation.", fp_adapter.sig.ident)))?;
+    let fp_adapter_body = fp_adapter.default.as_ref().ok_or_else(|| new_error(format!("{} must have an implementation.", fp_adapter.sig.ident)))?;
 
     Ok((parse_quote! {
         #trait_vis trait #fp_adapter_trait_name {
@@ -180,143 +239,18 @@ fn fp_adapter_trait(fp_adapter: &TraitItemFn, trait_name: &Ident, trait_vis: &Vi
 
 
 
-#[allow(clippy::manual_inspect)]
-fn find_func_by_attr<'a>(items: impl Iterator<Item= &'a mut TraitItem>, attr:&Attribute) -> impl Iterator<Item = &'a TraitItemFn> {
+#[expect(clippy::manual_inspect, reason = "inspect mut is not in std")]
+fn find_func_by_attr<'item>(items: impl Iterator<Item= &'item mut TraitItem>, attr:&Attribute) -> impl Iterator<Item = &'item TraitItemFn> {
     items.filter_map(|item| if let TraitItem::Fn(func) = item { Some(func)} else {None})
             .filter(|func| func.attrs.contains(attr))
             .map(move |func| { func.attrs.retain(|attrib| attrib != attr); func})
             .map(|func| &*func)
 }
 
-trait ArrayIter: Iterator {
-    fn collect_array<const N: usize>(self) -> Result<[Self::Item;N], ArrayBoundsError> where Self: Sized{
-        let collector: ArrayCollector<_,_> = self.collect();
-        collector.0
-    }
-}
-
-impl<T> ArrayIter for T where T: Iterator + ?Sized {}
-
-struct ArrayCollector<T, const N:usize>(Result<[T;N], ArrayBoundsError>);
-
-
-impl<T, const N:usize> FromIterator<T> for ArrayCollector<T, N> {
-    fn from_iter<B: IntoIterator<Item = T>>(iter: B) -> Self {
-        // SAFETY: The result items are guaranteed to be uninit until the for loop.
-        // After the ith iteration indices from 0 to i are guaranteed to be init by passing through the if branch.
-        // In case the iterator ends before N elements got initialized the first i elements that are init get droped.
-        // If the iterator contains more then N elements all elements in result get droped.
-        // Once the iter is empty and N(all) elements in the array got initialized it is safe to map all elements with assume_init.
-        let mut result = array::from_fn::<_, N,_>(|_| MaybeUninit::<T>::uninit());
-        let mut iter = iter.into_iter();
-        for i in 0..N {
-            let item = iter.next();
-            if let Some(item) = item {
-                result[i] = MaybeUninit::new(item);
-            } else {
-                result.iter_mut().take(i).for_each(|init_ele| unsafe { init_ele.assume_init_drop()});
-                return ArrayCollector(Err(ArrayBoundsError::Only(i, N)));
-            }
-        }
-        if iter.next().is_some() {
-            result.iter_mut().for_each(|init_ele| unsafe { init_ele.assume_init_drop()});
-            return ArrayCollector(Err(ArrayBoundsError::MoreThan(N)));
-        } 
-        ArrayCollector(Ok(result.map(|init_ele| unsafe { init_ele.assume_init()})))
-    }
-}
-
-
-
-enum ArrayBoundsError {
-    Only(usize, usize),
-    MoreThan(usize)
-}
-
-impl Display for ArrayBoundsError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ArrayBoundsError::Only(only, from) => write!(f, "Expected {from} element(s), found {only}."),
-            ArrayBoundsError::MoreThan(from) => write!(f, "Expected {from} elements, found {} or more.", from+1)
-        }
-    }
-}
-
 fn annotation_error(annotation:&str) -> impl Fn(ArrayBoundsError) -> TokenStream {
-    move |e| new_error(format!("Invalid number of #[{annotation}] annotated elements. {e}"))
+    move |error| new_error(format!("Invalid number of #[{annotation}] annotated elements. {error}"))
 }
 
-
-// fn fn_trait_result(mut item: ItemTrait) -> Result<TokenStream, TokenStream> {
-//     let adapter = find_func(&item, "adapter")?;
-//     let safe = find_func(&item, "safe")?;
-//     let safe_generics = safe.sig.generics.clone();
-//     let adapter_generics = adapter.sig.generics.clone();
-//     let adapter_fp_type = sig_to_fp(adapter.sig)?;
-//     let safe_fp_type = sig_to_fp(safe.sig)?;
-//     let mut call_site_adapter_generics = adapter_generics.clone();
-//     call_site_adapter_generics.params.iter_mut().for_each(|param| if let GenericParam::Type(t) = param {
-//         t.colon_token = None;
-//         t.bounds = Punctuated::default();
-//         t.default = None;
-//     });
-//     let params = call_site_adapter_generics.params;
-//     let adapter_func = parse_quote! {
-//         fn unsafe_fp #adapter_generics () -> #adapter_fp_type {
-//             Self::adapter::<#params>
-//         }
-//     };
-//     let safe_func = parse_quote! {
-//         fn safe_fp #safe_generics () -> #safe_fp_type {
-//             Self::safe
-//         }
-//     };
-
-//     item.items.append(&mut vec![adapter_func, safe_func]);
-
-
-
-//     let trait_vis = item.vis.clone();
-//     let trait_name = item.ident.clone();
-//     let safe_fp_type = create_type(&trait_vis, &trait_name, "SafeFP", &safe_generics, &safe_fp_type);
-//     let adapter_fp_type = create_type(&trait_vis, &trait_name, "UnsafeFP", &Generics::default(), &adapter_fp_type);
-
-
-//     let from_fp = find_func(&item, "from_fp")?;
-//     item.items.retain(|item| *item != TraitItem::Fn(from_fp.clone()));
-//     let to_safe_trait_name = Ident::new(&format!("{}ToSafe", trait_name), Span::call_site().into());
-
-//     let from_fp_generics = from_fp.sig.generics;
-//     let from_fp_return_type = from_fp.sig.output;
-//     let from_fp_body = from_fp.default.ok_or(new_error("from_fp needs to have a block."))?;
-
-//     let to_safe_trait:ItemTrait = parse_quote! {
-//         #trait_vis trait #to_safe_trait_name {
-//             fn to_safe #from_fp_generics (self) #from_fp_return_type;
-//         } 
-//     };
-
-//     let from_fp_receiver = from_fp.sig.inputs.first().ok_or(new_error("from_fp needs to take a parameter."))?;
-//     let FnArg::Receiver(from_fp_receiver) = from_fp_receiver else {
-//         return Err(new_error("from_fp needs to take a self parameter."));
-//     };
-//     let from_fp_receiver_type_name = from_fp_receiver.ty.clone();
-//     //let adapter_fp_type_name = adapter_fp_type.ident.clone();
-
-//     let to_safe_impl: ItemImpl = parse_quote! {
-//         impl #to_safe_trait_name for #from_fp_receiver_type_name {
-//             fn to_safe #from_fp_generics (self) #from_fp_return_type #from_fp_body
-//         }
-//     };
-
-//     Ok(quote! { 
-//         #item
-//         #safe_fp_type
-//         #adapter_fp_type
-//         #to_safe_trait
-//         #to_safe_impl
-//     }.into())
-// }
 
 fn create_type(vis: &Visibility, trait_name: &Ident, suffix:&str,  generics: &Generics, fp_type: &TypeBareFn) -> ItemType {
     let name = format!("{trait_name}{suffix}");
@@ -324,22 +258,6 @@ fn create_type(vis: &Visibility, trait_name: &Ident, suffix:&str,  generics: &Ge
     parse_quote! { #[allow(type_alias_bounds)]#vis type #ident #generics = #fp_type; }
 
 }
-
-// fn find_func(item: &ItemTrait, name: &str) -> Result<TraitItemFn, TokenStream> {
-//     item.items
-//         .iter()
-//         .find_map(|item| {
-//             if let TraitItem::Fn(func) = item {
-//                 if func.sig.ident.to_string() == name {
-//                     return Some(func.clone());
-//                 }
-//             }
-//             None
-//         })
-//         .ok_or(new_error(&format!(
-//             "Trait must contain function named '{name}'"
-//         )))
-// }
 
 fn sig_to_fp(sig: Signature) -> Result<TypeBareFn, TokenStream> {
     let Signature {
@@ -365,6 +283,7 @@ fn sig_to_fp(sig: Signature) -> Result<TypeBareFn, TokenStream> {
     })
 }
 
+#[expect(clippy::single_call_fn, reason = "function extracted for visibility")]
 fn args_to_bare_args(
     args: Punctuated<FnArg, Comma>,
 ) -> Result<Punctuated<BareFnArg, Comma>, TokenStream> {
@@ -382,6 +301,7 @@ fn args_to_bare_args(
         .collect()
 }
 
+#[expect(clippy::single_call_fn, reason = "function extracted for visibility")]
 fn variadic_to_bare_variadic(variadic: Option<Variadic>) -> Option<BareVariadic> {
     let Variadic {
         attrs, dots, comma, ..
