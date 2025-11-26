ca1988a55d96c625f2cce80454f1dd77d2718503
RedIODev <redio.development@gmail.com>
2025-11-27 00:29:10 +0100

    fixed all warnings in example

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,59 +1,64 @@
+#![allow(clippy::print_stdout, reason = "plugins are allowed to print to std_out/err")]
+#![allow(clippy::use_debug, reason = "example is allowed to print debug info")]
+
+
 extern crate alloc;
 use alloc::borrow::Cow;
-
+use alloc::sync::Arc;
+use core::sync::atomic::Ordering;
+use core::time::Duration;
 use std::{
-    io::{Write, stdin, stdout},
-    sync::{Arc, atomic::Ordering},
+    io::{Write as _, stdin, stdout},
     thread,
-    time::Duration,
 };
 
 use arc_swap::ArcSwapOption;
 use atomic_enum::atomic_enum;
 use plugin_loader_api::{
-    API_VERSION, ApplicationContext, ErrorMapper, PluginInfo, ServiceError,
+    API_VERSION, ApplicationContext, ErrorMapper as _, PluginInfo, ServiceError,
     pointer_traits::{EventHandlerFunc, plugin_main, trait_fn},
 };
 use serde::Deserialize;
 use serde_json::json;
 use uuid::Uuid;
 
+static UUID: ArcSwapOption<Uuid> = ArcSwapOption::const_empty();
+static POWER: AtomicMyPow = AtomicMyPow::new(MyPow::None);
+
 #[atomic_enum]
 #[derive(Deserialize, PartialEq, PartialOrd)]
 #[serde(rename_all = "lowercase")]
 enum MyPow {
+    Cancel,
     #[serde(skip)]
     None, 
-    Cancel,
-    Shutdown,
     Restart,
+    Shutdown,
 }
 
 #[derive(Deserialize, PartialEq)]
 struct PowWrap {
     command: MyPow,
-    timestamp: String,
     delay: Option<u32>,
+    timestamp: String,
 }
 
+#[expect(clippy::single_call_fn, reason = "main function")]
 #[plugin_main]
 fn main(uuid: Uuid) -> PluginInfo {
-    println!("Main: Test from plugin! {uuid:?}");
+    println!("Main: Test from plugin! {uuid}");
     UUID.store(Some(Arc::new(uuid)));
     PluginInfo::new::<InitTest, _, _, _>("ExamplePlugin", "0.0.2", [], API_VERSION)
 }
 
-static POWER: AtomicMyPow = AtomicMyPow::new(MyPow::None);
-static UUID: ArcSwapOption<Uuid> = ArcSwapOption::const_empty();
-
 #[trait_fn(EventHandlerFunc for PowerListener)]
-fn handle<'a, F: Fn() -> Result<ApplicationContext, ServiceError>, S: Into<Cow<'a, str>>>(
+fn handle<'args, F: Fn() -> Result<ApplicationContext, ServiceError>, S: Into<Cow<'args, str>>>(
     context: F,
     args: S,
 ) -> Result<(), ServiceError> {
-    let args: PowWrap = serde_json::from_str(&args.into()).error(ServiceError::InvalidString)?;
-    POWER.store(args.command, Ordering::Relaxed);
-    if args.delay.is_some() {
+    let pow_args: PowWrap = serde_json::from_str(&args.into()).error(ServiceError::InvalidString)?;
+    POWER.store(pow_args.command, Ordering::Relaxed);
+    if pow_args.delay.is_some() {
         let uuid = **UUID.load().as_ref().error(ServiceError::PluginInternalError)?;
         context()?.endpoint_request("core:power", uuid, json!({"command": "cancel"}).to_string())?;
         println!("canceled shutdown");
@@ -62,7 +67,7 @@ fn handle<'a, F: Fn() -> Result<ApplicationContext, ServiceError>, S: Into<Cow<'
 }
 
 #[trait_fn(EventHandlerFunc for InitTest)]
-fn handle<'a, F: Fn() -> Result<ApplicationContext, ServiceError>, S: Into<Cow<'a, str>>>(context: F, args: S) -> Result<(), ServiceError> {
+fn handle<'args, F: Fn() -> Result<ApplicationContext, ServiceError>, S: Into<Cow<'args, str>>>(context: F, args: S) -> Result<(), ServiceError> {
     
         println!("Plugin: Init: Test from plugin! Args:{}", args.into());
         let uuid = **UUID.load().as_ref().error(ServiceError::PluginInternalError)?;
@@ -74,12 +79,12 @@ fn handle<'a, F: Fn() -> Result<ApplicationContext, ServiceError>, S: Into<Cow<'
             stdout().flush().error(ServiceError::PluginInternalError)?;
             stdin().read_line(&mut input).error(ServiceError::PluginInternalError)?;
             println!("after read");
-            let args = if input.trim() == "shutdown20000" {
-                json!({"command": "shutdown", "delay": 20000}).to_string()
+            let request_args = if input.trim() == "shutdown20000" {
+                json!({"command": "shutdown", "delay": 20_000i32}).to_string()
             } else {
                 json!({"command": input.trim()}).to_string()
             };
-            match context()?.endpoint_request("core:power", uuid, args) {
+            match context()?.endpoint_request("core:power", uuid, request_args) {
                 Ok(response) => println!("Response:{response}"),
                 Err(err) => println!("RequestError:{err}")
             }
